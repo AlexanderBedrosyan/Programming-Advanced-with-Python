edcb347632d2ff52deb0ef6a13645d13303c603d
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2023-09-26 08:25:55 +0300

    Solutions for Functions Advanced - Lab
    Solutions for Functions Advanced - Lab

---
+++ Functions Advanced - Lab/multiplication_function.py
@@ -0,0 +1,11 @@
+def multiply(*args):
+    number = 1
+    for ch in args:
+        number *= ch
+
+    return number
+
+
+print(multiply(1, 4, 5))
+print(multiply(4, 5, 6, 1, 3))
+print(multiply(2, 0, 1000, 5000))
